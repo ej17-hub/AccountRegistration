46b85b929357dd5152066840741735823948f566
eljohnchrisespiritu@gmail.com <eljohnchrisespiritu@gmail.com>
2025-08-15 13:13:14 +0800

    Add project files.

---
+++ AccountRegistration/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace AccountRegistration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxStudNum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxLName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxFName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxMName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxContNum = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.comboBoxProgram = new System.Windows.Forms.ComboBox();
+            this.TextBoxAddress = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student No.:";
+            // 
+            // textBoxStudNum
+            // 
+            this.textBoxStudNum.Location = new System.Drawing.Point(31, 55);
+            this.textBoxStudNum.Name = "textBoxStudNum";
+            this.textBoxStudNum.Size = new System.Drawing.Size(161, 20);
+            this.textBoxStudNum.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Program:";
+            // 
+            // textBoxLName
+            // 
+            this.textBoxLName.Location = new System.Drawing.Point(31, 132);
+            this.textBoxLName.Name = "textBoxLName";
+            this.textBoxLName.Size = new System.Drawing.Size(161, 20);
+            this.textBoxLName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Last Name:";
+            // 
+            // textBoxFName
+            // 
+            this.textBoxFName.Location = new System.Drawing.Point(271, 132);
+            this.textBoxFName.Name = "textBoxFName";
+            this.textBoxFName.Size = new System.Drawing.Size(161, 20);
+            this.textBoxFName.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(268, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "First Name:";
+            // 
+            // textBoxMName
+            // 
+            this.textBoxMName.Location = new System.Drawing.Point(530, 132);
+            this.textBoxMName.Name = "textBoxMName";
+            this.textBoxMName.Size = new System.Drawing.Size(161, 20);
+            this.textBoxMName.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(527, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Middle Name:";
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(31, 219);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(161, 20);
+            this.textBoxAge.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Age:";
+            // 
+            // textBoxContNum
+            // 
+            this.textBoxContNum.Location = new System.Drawing.Point(271, 219);
+            this.textBoxContNum.Name = "textBoxContNum";
+            this.textBoxContNum.Size = new System.Drawing.Size(161, 20);
+            this.textBoxContNum.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(268, 203);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Contact No.:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(28, 267);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Address:";
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(321, 428);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 17;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // comboBoxProgram
+            // 
+            this.comboBoxProgram.FormattingEnabled = true;
+            this.comboBoxProgram.Items.AddRange(new object[] {
+            "Bachelor of Science in Information Technology (BSIT)",
+            "Bachelor of Computer Science (BSCS)",
+            "Bachelor of Science in Information Systems (BSIS)",
+            "Bachelor of Science in Computer Engineering (BSCpE)",
+            "Bachelor of Science in Data Science"});
+            this.comboBoxProgram.Location = new System.Drawing.Point(271, 54);
+            this.comboBoxProgram.Name = "comboBoxProgram";
+            this.comboBoxProgram.Size = new System.Drawing.Size(268, 21);
+            this.comboBoxProgram.TabIndex = 18;
+            // 
+            // TextBoxAddress
+            // 
+            this.TextBoxAddress.Location = new System.Drawing.Point(31, 283);
+            this.TextBoxAddress.Multiline = true;
+            this.TextBoxAddress.Name = "TextBoxAddress";
+            this.TextBoxAddress.Size = new System.Drawing.Size(699, 139);
+            this.TextBoxAddress.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 463);
+            this.Controls.Add(this.TextBoxAddress);
+            this.Controls.Add(this.comboBoxProgram);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxContNum);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxMName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxFName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxLName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxStudNum);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "sni";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxStudNum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxLName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxFName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxMName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxContNum;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.ComboBox comboBoxProgram;
+        private System.Windows.Forms.TextBox TextBoxAddress;
+    }
+}
+
---
+++ AccountRegistration/Form2.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace AccountRegistration
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblform2StudNum = new System.Windows.Forms.Label();
+            this.lblform2Program = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblform2LName = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblform2FName = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblform2MName = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblform2Age = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lblform2ContactNum = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.lblform2Address = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(274, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Number:";
+            // 
+            // lblform2StudNum
+            // 
+            this.lblform2StudNum.AutoSize = true;
+            this.lblform2StudNum.Location = new System.Drawing.Point(399, 38);
+            this.lblform2StudNum.Name = "lblform2StudNum";
+            this.lblform2StudNum.Size = new System.Drawing.Size(41, 13);
+            this.lblform2StudNum.TabIndex = 1;
+            this.lblform2StudNum.Text = "No info";
+            // 
+            // lblform2Program
+            // 
+            this.lblform2Program.AutoSize = true;
+            this.lblform2Program.Location = new System.Drawing.Point(399, 76);
+            this.lblform2Program.Name = "lblform2Program";
+            this.lblform2Program.Size = new System.Drawing.Size(41, 13);
+            this.lblform2Program.TabIndex = 3;
+            this.lblform2Program.Text = "No info";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(274, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Program:";
+            // 
+            // lblform2LName
+            // 
+            this.lblform2LName.AutoSize = true;
+            this.lblform2LName.Location = new System.Drawing.Point(399, 113);
+            this.lblform2LName.Name = "lblform2LName";
+            this.lblform2LName.Size = new System.Drawing.Size(41, 13);
+            this.lblform2LName.TabIndex = 5;
+            this.lblform2LName.Text = "No info";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(274, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Last Name:";
+            // 
+            // lblform2FName
+            // 
+            this.lblform2FName.AutoSize = true;
+            this.lblform2FName.Location = new System.Drawing.Point(399, 152);
+            this.lblform2FName.Name = "lblform2FName";
+            this.lblform2FName.Size = new System.Drawing.Size(41, 13);
+            this.lblform2FName.TabIndex = 7;
+            this.lblform2FName.Text = "No info";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(274, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "First Name:";
+            // 
+            // lblform2MName
+            // 
+            this.lblform2MName.AutoSize = true;
+            this.lblform2MName.Location = new System.Drawing.Point(399, 191);
+            this.lblform2MName.Name = "lblform2MName";
+            this.lblform2MName.Size = new System.Drawing.Size(41, 13);
+            this.lblform2MName.TabIndex = 9;
+            this.lblform2MName.Text = "No info";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(274, 191);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(72, 13);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Middle Name:";
+            // 
+            // lblform2Age
+            // 
+            this.lblform2Age.AutoSize = true;
+            this.lblform2Age.Location = new System.Drawing.Point(399, 231);
+            this.lblform2Age.Name = "lblform2Age";
+            this.lblform2Age.Size = new System.Drawing.Size(41, 13);
+            this.lblform2Age.TabIndex = 11;
+            this.lblform2Age.Text = "No info";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(274, 231);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(29, 13);
+            this.label12.TabIndex = 10;
+            this.label12.Text = "Age:";
+            // 
+            // lblform2ContactNum
+            // 
+            this.lblform2ContactNum.AutoSize = true;
+            this.lblform2ContactNum.Location = new System.Drawing.Point(399, 272);
+            this.lblform2ContactNum.Name = "lblform2ContactNum";
+            this.lblform2ContactNum.Size = new System.Drawing.Size(41, 13);
+            this.lblform2ContactNum.TabIndex = 13;
+            this.lblform2ContactNum.Text = "No info";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(274, 272);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(87, 13);
+            this.label14.TabIndex = 12;
+            this.label14.Text = "Contact Number:";
+            // 
+            // lblform2Address
+            // 
+            this.lblform2Address.AutoSize = true;
+            this.lblform2Address.Location = new System.Drawing.Point(399, 314);
+            this.lblform2Address.Name = "lblform2Address";
+            this.lblform2Address.Size = new System.Drawing.Size(41, 13);
+            this.lblform2Address.TabIndex = 15;
+            this.lblform2Address.Text = "No info";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(274, 314);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(48, 13);
+            this.label16.TabIndex = 14;
+            this.label16.Text = "Address:";
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(330, 381);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(86, 28);
+            this.buttonSubmit.TabIndex = 16;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.lblform2Address);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.lblform2ContactNum);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.lblform2Age);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.lblform2MName);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblform2FName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblform2LName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblform2Program);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblform2StudNum);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "FrmConfirm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblform2StudNum;
+        private System.Windows.Forms.Label lblform2Program;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblform2LName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblform2FName;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblform2MName;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblform2Age;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lblform2ContactNum;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label lblform2Address;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Button buttonSubmit;
+    }
+}
